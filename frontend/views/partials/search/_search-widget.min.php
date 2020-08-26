3fd90f60fa12c5a20682421525583a1d9a42784d
vkponomarev <vkponomarev@gmail.com>
2020-08-27 01:16:09 +0500

    Доработка search виджета

--- frontend/views/partials/search/_search-widget.min.php
+++ frontend/views/partials/search/_search-widget.min.php
@@ -1,40 +1,23 @@
 <div><?php
-
     $template = '<div class="search-value-div"><p class="search-value">{{value}}</p>';
-
     echo \kartik\typeahead\Typeahead::widget([
         'name' => 'typeahead',
         'options' => ['placeholder' => Yii::t('app','Search ...')
         ],
         'pluginOptions' => ['hint' => true, 'highlight' => true, 'minLength' => 3],
         'pluginEvents' => [
-            //"typeahead:select" => "function(ev, suggestion) {
-            //window.location = '/artists/' + suggestion.url; }"
-
-            //"typeahead:select" => "function(ev, suggestion) {
-            //console.log(suggestion.url) }"
-
             "typeahead:select" => "function(ev, suggestion) {
-         
                 if (typeof suggestion.url === \"undefined\" || suggestion.url === null || suggestion.url ===  \"\") {
-        
-                    //здесь можно открыть страницу поиска
-                    
+                    //here u can open search page
                 } else {
-                
                     window.location = '/" . Yii::$app->language . "/' + suggestion.url + '/';
-                
                 }
-               
              }"
-
         ],
         'dataset' => [
             [
                 'datumTokenizer' => "Bloodhound.tokenizers.obj.whitespace('value')",
                 'display' => 'value',
-                //'prefetch' => $baseUrl . '/samples/countries.json',
-                //'prefetch' => \yii\helpers\Url::to(['search/search']),
                 'limit' => 10,
                 'templates' => [
                     'notFound' => '<div class="text-danger" style="padding:0 8px">' . Yii::t('app','No matches found.') . '</div>',
@@ -43,15 +26,8 @@
                 'remote' => [
                     'url' => \yii\helpers\Url::to(['search/search']) . '?q=%QUERY',
                     'wildcard' => '%QUERY'
-
                 ]
-
             ],
-
         ]
     ]);
-
-    //echo \yii\helpers\Url::to(['search/search']) . '?q=%QUERY';
-
-
     ?></div>
